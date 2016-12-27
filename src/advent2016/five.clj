270f242d26bb711a743a2202be2c2c18197d4603
jcurtis <jamocurtis@gmail.com>
2016-12-27 16:02:36 -0500

    solve 5

--- src/advent2016/five.clj
+++ src/advent2016/five.clj
@@ -1,7 +1,9 @@
 (ns advent2016.five
   (:import
         [java.security MessageDigest]
-        [java.math BigInteger]))
+        [java.math BigInteger])
+  (:require
+        [advent2016.util :as util]))
 
 (defn md5 [s]
   (let [algorithm (MessageDigest/getInstance "MD5")
@@ -11,12 +13,52 @@
         padding (apply str (repeat (- size (count sig)) "0"))]
     (str padding sig)))
 
-(defn hash-at
-  [string index]
-  (md5 (str string index)))
-
 (defn is-valid
   [hash]
   (= (.substring hash 0 5) "00000"))
 
-; can't figure out the lazy sequence thing
+(defn next-hash
+  [input x]
+  (loop [n x]
+    (let [hash (md5 (str input n))]
+      (if (is-valid hash)
+        {:count n :hash hash}
+        (recur (inc n))))))
+
+(defn solve1
+  [input]
+  (loop [n 0
+         counter 0
+         acc ""]
+    (if (>= n 8)
+      acc
+      (let [next (next-hash input counter)]
+        (do
+          (println next)
+          (recur (inc n) (inc (:count next)) (str acc (.charAt (:hash next) 5))))))))
+
+; (solve1 "abc") ;=> 18f47a30
+; (solve1 "cxdnnyjw")
+
+(defn solve2
+  [input]
+  (loop [counter 0
+         acc [nil nil nil nil nil nil nil nil]]
+    (if (not-any? nil? acc)
+      (apply str acc)
+      (let [{count :count hashed :hash} (next-hash input counter)]
+        (do
+          (println "hash" hashed "count" count)
+          (println "acc" acc)
+          (recur
+            (inc count)
+            (let [sixth (and (re-matches #"[0-7]" (str (.charAt hashed 5))) (util/parse-int (.charAt hashed 5)))
+                  seventh (.charAt hashed 6)]
+              (do
+                (println "sixth" sixth "seventh" seventh)
+                (if (and (not (nil? sixth)) (nil? (acc sixth)))
+                  (assoc acc sixth seventh)
+                  acc)))))))))
+
+; (solve2 "abc") ;=> 05ace8e3
+; (solve2 "cxdnnyjw")
