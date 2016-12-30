f69349aeffbfa9fc824662b6e1b0b0e8c0b714df
jcurtis <jamocurtis@gmail.com>
2016-12-30 15:49:49 -0500

    solve fourteen part 1

---
+++ src/advent2016/fourteen.clj
@@ -0,0 +1,28 @@
+(ns advent2016.fourteen
+  (:require [advent2016.util :as util]))
+
+(defn gen-hash-seq
+  ([salt] (gen-hash-seq salt 0))
+  ([salt i]
+   (lazy-seq
+    (cons
+      (util/md5 (str salt i))
+      (gen-hash-seq salt (inc i))))))
+
+(defn has-triple? [hash] (re-find #"([a-z0-9])\1\1" hash))
+(defn has-quintuple? [hash char] (re-find (re-pattern (apply str (repeat 5 char))) hash))
+
+(defn one-time-pads
+  [salt]
+  (let [hash-seq (gen-hash-seq salt)]
+    (keep-indexed
+      (fn [idx hash]
+        (let [[_ char] (has-triple? hash)]
+          (and char
+            (let [quints (keep-indexed #(if (has-quintuple? %2 char) [(+ %1 idx) %2] nil) (take 1000 (drop (inc idx) hash-seq)))]
+              (if (empty? quints) nil {:idx idx :hash hash :quints quints})))))
+      hash-seq)))
+
+(defn solve1
+  [salt]
+  (nth (one-time-pads salt) 63))
